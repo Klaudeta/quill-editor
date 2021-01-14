a6f20e0781cf4076eaa48d4a2de075bc1bad29b5
lmorocz <laszlo_morocz@rrsoftware.hu>
2021-01-14 19:09:43 +0100

    Fixing the faulty QuillEditorComponent#setHtmlContent JS call + other fixes
    1. Replaced the `callJsFunction` call in QuillEditorComponent.java which should
    be an `executeJs` one with the given JS code and parameters. The callJsFunction
    version called: `return $0.$0.setHtml($1)($1,$2)`. :o
    
    2. Fixed `quill` references to `this.quillEditor` in quilleditor.js to prevent
    JS errors
    
    3. Changed `vaadin` dependency to `vaadin-core` in pom.xml to exclude commercial
    Vaadin packages from the project
    
    4. Added clear and "readonly toggle" buttons to test views. Clear was definitely
    broken with the `callJsFunction` version. With the new buttons one can tests these
    features too.
    
    5. Added `overflow: auto;` to the readonly editor (div) class to prevent text
    overflow in readonly editor mode

--- src/main/java/org/vaadin/klaudeta/quill/QuillEditorComponent.java
+++ src/main/java/org/vaadin/klaudeta/quill/QuillEditorComponent.java
@@ -1,5 +1,7 @@
 package org.vaadin.klaudeta.quill;
 
+import java.util.Objects;
+
 import com.vaadin.flow.component.ClientCallable;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasComponents;
@@ -13,12 +15,10 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.dom.DisabledUpdateMode;
 import com.vaadin.flow.function.SerializableConsumer;
-import java.util.Objects;
 
 /**
  * A custom RichText editor component for Flow using Quill library.
  */
-
 @Tag("quill-editor")
 @NpmPackage(value = "lit-element", version = "^2.2.1")
 @NpmPackage(value = "lit-html", version = "^1.1.2")
@@ -49,9 +49,7 @@ public class QuillEditorComponent extends Component implements HasComponents, Qu
         editor.setId("editor-quill");
         add(editor);
         this.getElement().executeJs("$0.initEditor($1)",this, editor.getElement());
-
     }
-
 
     @ClientCallable(DisabledUpdateMode.ALWAYS)
     private void setHtml(String htmlContent) {
@@ -85,14 +83,14 @@ public class QuillEditorComponent extends Component implements HasComponents, Qu
         if(!Objects.equals(oldContent, htmlContent)){
             this.htmlContent = htmlContent;
             runBeforeClientResponse(ui -> {
-                editor.getElement().callJsFunction("$0.setHtml($1)", this,  htmlContent);
+                editor.getElement().executeJs("$0.setHtml($1)", this,  htmlContent);
             });
         }
-
     }
 
     private void runBeforeClientResponse(SerializableConsumer<UI> command) {
         getElement().getNode().runWhenAttached(ui -> ui
                 .beforeClientResponse(this, context -> command.accept(ui)));
     }
+
 }
